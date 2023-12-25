dc61a9e0ecfa4a8266a62221f658db5b0b70047f
Anurag30112003 <anuragsharma011011@gmail.com>
2023-12-26 05:06:50 +0530

    feat:- added admin email to runtime env

--- src/env.js
+++ src/env.js
@@ -57,6 +57,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     MODERATOR_KEY: process.env.MODERATOR_KEY,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
