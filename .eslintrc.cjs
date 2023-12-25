bf3abb90f62e939cdb3fd1177162c3e2326b5d59
Dhravya <dhravyashah@gmail.com>
2023-12-25 15:36:35 -0700

    added 'no-floating-promises' rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-floating-promises": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
