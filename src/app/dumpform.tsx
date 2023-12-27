a1d2729b133859a52a21a1c5898d7d702e8a272f
Dhravya <dhravyashah@gmail.com>
2023-12-27 14:35:32 -0700

    operator precedence fix

--- src/app/dumpform.tsx
+++ src/app/dumpform.tsx
@@ -8,7 +8,6 @@ import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 
 function DumpForm({ className }: { className?: string }) {
-
   const [isDumping, setIsDumping] = useState(false);
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
@@ -18,14 +17,13 @@ function DumpForm({ className }: { className?: string }) {
     setIsDumping(true);
     document.querySelector(".loader")!.classList.add("loading");
 
-    const dump = (
-      document.getElementById("dumpcontent") as HTMLInputElement
-    ).value;
+    const dump = (document.getElementById("dumpcontent") as HTMLInputElement)
+      .value;
 
     const isPublic =
-      (
-        document.getElementById("isPrivate") as HTMLInputElement
-      ).getAttribute("data-state") == "checked";
+      (document.getElementById("isPrivate") as HTMLInputElement).getAttribute(
+        "data-state",
+      ) == "checked";
 
     await createDump(dump, isPublic);
 
@@ -35,16 +33,15 @@ function DumpForm({ className }: { className?: string }) {
     setIsDumping(false);
     document.querySelector(".loader")!.classList.add("complete");
     document.querySelector(".loader")!.classList.remove("loading");
-    
+
     setTimeout(() => {
       document.querySelector(".loader")!.classList.remove("complete");
     }, 1000);
-  }
+  };
 
   useEffect(() => {
     function onKeyDown(e: KeyboardEvent) {
-      if ((/[a-zA-Z]/).test(e.key) && !e.ctrlKey && !e.shiftKey && !e.altKey) {
-        
+      if (/[a-zA-Z]/.test(e.key) && !e.ctrlKey && !e.shiftKey && !e.altKey) {
         inputRef.current!.focus();
       }
     }
@@ -53,23 +50,31 @@ function DumpForm({ className }: { className?: string }) {
 
     return () => {
       document.removeEventListener("keydown", onKeyDown);
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <form
       onSubmit={submitDump}
       className={`mt-4 w-full ${className}`}
       onKeyDown={(e) => {
-        if (e.key == "Enter" && e.ctrlKey || e.metaKey) {
-          e.preventDefault()
+        if (e.key == "Enter" && (e.ctrlKey || e.metaKey)) {
+          e.preventDefault();
           submitDump(e);
         }
       }}
     >
-      <Textarea ref={inputRef} required id="dumpcontent" placeholder="What's on your mind?" disabled={isDumping} />
-      <div className="mt-4 flex justify-between flex-row-reverse">
-        <Button type="submit" disabled={isDumping}>Dump</Button>
+      <Textarea
+        ref={inputRef}
+        required
+        id="dumpcontent"
+        placeholder="What's on your mind?"
+        disabled={isDumping}
+      />
+      <div className="mt-4 flex flex-row-reverse justify-between">
+        <Button type="submit" disabled={isDumping}>
+          Dump
+        </Button>
         <div className="flex items-center gap-2">
           <Switch defaultChecked={true} id="isPrivate" disabled={isDumping} />
           <Label htmlFor="isPrivate">Public</Label>
