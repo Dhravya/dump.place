cdb52ee4a5a2d14df395989916b1716cccab5fb8
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-31 17:22:01 -0800

    feat(dumps): add DumpSkeleton loader

---
+++ src/components/skeletons/dumpskeleton.tsx
@@ -0,0 +1,47 @@
+function DumpSkeleton() {
+  return (
+    <div className="w-full flex-col items-start justify-start gap-2 rounded-2xl border-[0.1px] border-black/50 bg-gradient-to-tr from-purple-400/10 via-gray-500/5 to-transparent p-4 px-7 py-5 shadow-zinc-500 transition-[border-color] dark:border-white/20 dark:shadow-purple-400">
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex w-full flex-col">
+          <div className="flex w-full items-center gap-2">
+            {/* avatar skeleton */}
+            <div className="h-7 w-7 animate-pulse rounded-full bg-white/15"></div>
+            <div className="flex w-full flex-col gap-2">
+              {/* author skeleton */}
+              <div className="h-4 w-2/5 animate-pulse rounded-sm bg-white/15 md:w-3/5 xl:w-2/5"></div>
+              {/* date skeleton */}
+              <div className="h-3 w-1/5 animate-pulse rounded-sm bg-white/15 md:w-2/5 xl:w-1/5"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* body skeleton */}
+      <div className="mt-4 h-5 w-full animate-pulse rounded-sm bg-white/15"></div>
+    </div>
+  );
+}
+
+export default function DumpSkeletonWrapper() {
+  return (
+    <>
+      <h2 className="mb-8 text-center font-heading text-4xl  font-bold md:text-5xl">
+        Public{" "}
+        <span className="bg-gradient-to-tr from-purple-400/80 via-white to-white/90 bg-clip-text text-transparent">
+          Dumps
+        </span>
+      </h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+        <DumpSkeleton />
+      </div>
+    </>
+  );
+}
